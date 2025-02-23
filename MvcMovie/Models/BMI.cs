32d08bfa6d93c926c81ef83e84eb232b599a541f
trankhanhson0104 <khanhson010423@gmail.com>
2025-02-23 22:18:00 +0700

    Delete MvcMovie directory

--- MvcMovie/Models/BMI.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-namespace MvcMovie.Models;
-
-public class BMI 
-{
-    public float chieucao { get; set; }
-    public float cannang { get; set; }
-
-}
--- MvcMovie/Models/diemmonhoc.cs
+++
@@ -1,8 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-namespace MvcMovie.Models;
-public class Diemmh
-{
-    public double Diema { get; set; }
-    public double Diemb { get; set; }
-    public double Diemc { get; set; }
-}
