8261b2d32f807706f433f7132b38c86574243c3a
“trankhanhson010423 <khanhson010423@gmail.com>
2025-03-17 00:22:40 +0700

    luyentap

--- MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6099bd60ee3fba281851039ef7f94b605f8adc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e0b1225e387664da7591fae3923a827b86ec7bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
